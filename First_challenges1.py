08488335d51f5814bf90cc06c331d2996a260d77
Thubs <hughcookpalmeh@gmail.com>
2022-05-08 03:09:48 -0800

    Adding files from a private repository

---
+++ First_challenges1.py
@@ -0,0 +1,49 @@
+# Challenge 1
+
+print("hello world")
+
+# Challenge 2
+
+print("the fakers gonna")
+
+fake = ('fake')
+
+print(fake)
+print(fake)
+print(fake)
+print(fake)
+print(fake)
+
+# Challenge 3
+
+local_people = input('Who are the local people: ')
+
+print("greetings, " + local_people + "!")
+
+# Challenge 4
+
+end_station = input("End station: ")
+
+print("This is the " + end_station + " train service")
+print("This train will terminate at " + end_station)
+
+# Challenge 5
+
+animal1 = input("What is an animal: ")
+animal2 = input("What is another animal: ")
+
+print("I see a " + animal1 + " " + animal2 + " over there!")
+
+# Challenge 6
+
+# Make sure you pay attention to the int
+numberino = int(input("Input a number to be doubled: "))
+
+doubled = numberino * 2
+
+print(doubled)
+
+# infinite boob generator
+
+while 1>0:
+  print("Hehe boobs")
